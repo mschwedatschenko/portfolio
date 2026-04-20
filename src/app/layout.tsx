51628b8ecd99199cd0af85c62fa5340866814f34
mschwedatschenko <96269239+mschwedatschenko@users.noreply.github.com>
2026-04-20 16:31:18 -0400

    Add favicon - dan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,12 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang="en" className="dark scroll-smooth">
+			<head>
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+				<link rel="manifest" href="/favicon/site.webmanifest"/>
+			</head>
 			<body
 				className="min-h-screen bg-gradient-to-b from-slate-950 via-slate-950/95 to-slate-900 text-foreground antialiased"
 				style={{ fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif' }}
